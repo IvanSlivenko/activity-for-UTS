992ceebd184536936214aad4a985456ba4d324da
Ivan Slivenko <umanproger@ukr.net>
2026-08-18 18:03:48 +0300

    18-08-2026-3

--- Lessons about Kivy from John Elder/lesson_8.py
+++ Lessons about Kivy from John Elder/lesson_8.py
@@ -9,30 +9,11 @@ Builder.load_file('box.kv')
 
 
 class MyLayout(Widget):
+    pass
 
-    name = ObjectProperty(None)
-    pizza = ObjectProperty(None)
-    color = ObjectProperty(None)
-
-
-#-------------------------------------------------------------- actions
-    def press(self):
-        name = self.name.text
-        pizza = self.pizza.text
-        color = self.color.text
-
-        print(f'Hello {name}, you like {pizza}  pizza, and your favorite color is {color}!')
-        # self.add_widget(Label(text=f'Hello {name}, you like {pizza}  pizza, and your favorite color is {color}!'))
-
-        #Clear yhe input boxes
-        self.name.text=""
-        self.pizza.text = ""
-        self.color.text = ""
-
-#-------------------------------------------------------------- runner
-class AvesomeApp(App):
+class AwesomeApp(App):
     def build(self):
-        return MyGridLayout()
+        return MyLayout()
 
 if __name__ == "__main__":
-    AvesomeApp().run()
+    AwesomeApp().run()
