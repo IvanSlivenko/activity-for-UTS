4f9b609d087b4a302f493f7bade6cc89ba51e30f
Ivan Slivenko <umanproger@ukr.net>
2026-08-18 08:09:00 +0300

    18-08-2026-1

---
+++ Lessons about Kivy from John Elder/lesson_8.py
@@ -0,0 +1,38 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+
+Builder.load_file('box.kv')
+
+
+
+class MyLayout(Widget):
+
+    name = ObjectProperty(None)
+    pizza = ObjectProperty(None)
+    color = ObjectProperty(None)
+
+
+#-------------------------------------------------------------- actions
+    def press(self):
+        name = self.name.text
+        pizza = self.pizza.text
+        color = self.color.text
+
+        print(f'Hello {name}, you like {pizza}  pizza, and your favorite color is {color}!')
+        # self.add_widget(Label(text=f'Hello {name}, you like {pizza}  pizza, and your favorite color is {color}!'))
+
+        #Clear yhe input boxes
+        self.name.text=""
+        self.pizza.text = ""
+        self.color.text = ""
+
+#-------------------------------------------------------------- runner
+class AvesomeApp(App):
+    def build(self):
+        return MyGridLayout()
+
+if __name__ == "__main__":
+    AvesomeApp().run()
