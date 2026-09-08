685b1011cd146a3ddcffbca049ea9c34d0744a5e
Ivan Slivenko <umanproger@ukr.net>
2026-09-08 23:42:08 +0300

    08-09-2026-1

--- Lessons about Kivy from John Elder_2/24/lesson_24.py
+++ Lessons about Kivy from John Elder_2/24/lesson_24.py
@@ -6,6 +6,10 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.core.spelling import Spelling
 
+
+
+print("Spelling",Spelling)
+
 #from kivy.uix.floatlayout import FloatLayout
 #from kivy.uix.image import Image
 
@@ -18,9 +22,17 @@ Builder.load_file('spell_24.kv')
 
 class MyLayout(Widget):
     def press(self):
-        print(self.ids.word_input.text)
+        #print(self.ids.word_input.text)
+        s = Spelling()
+        #s.select_language('uk_UA')
+        s.select_language('en_US')
+        #print(s.list_languages())
 
+        word = self.ids.word_input.text
 
+        option = s.suggest(word)
+
+        self.ids.word_label.text = f'Suggestions: {option}'
 class AwesomeApp(App):
     def build(self):
         Window.clearcolor = (0,0,0,0)
