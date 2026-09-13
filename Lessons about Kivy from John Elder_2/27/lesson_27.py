bf52e475d6bdd4fa333b0e5aa1e19c155bd8a7c9
Ivan Slivenko <umanproger@ukr.net>
2026-09-13 14:07:53 +0300

    13-09-2026-1

---
+++ Lessons about Kivy from John Elder_2/27/lesson_27.py
@@ -0,0 +1,30 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.core.spelling import Spelling
+
+
+
+#print("Spelling",Spelling)
+
+#from kivy.uix.floatlayout import FloatLayout
+#from kivy.uix.image import Image
+
+#Set the app size
+Window.size = (700,500)
+
+Builder.load_file('carousel.kv')
+
+class MyLayout(Widget):
+    pass
+
+class AwesomeApp(App):
+    def build(self):
+        Window.clearcolor = (0,0,0,0)
+        return MyLayout()
+
+if __name__ == "__main__":
+    AwesomeApp().run()
