9d30f6a35a07cbda7ba9ca7ca7ee985d7734e757
Ivan Slivenko <umanproger@ukr.net>
2026-09-13 21:35:40 +0300

    13-09-2026-2

--- Lessons about Kivy from John Elder_2/28/lesson_28.py
+++ Lessons about Kivy from John Elder_2/28/lesson_28.py
@@ -10,7 +10,7 @@ from kivy.core.spelling import Spelling
 #Set the app size
 Window.size = (700,500)
 
-Builder.load_file('carousel.kv')
+Builder.load_file('CheckBoxes_28.kv.kv')
 
 class MyLayout(Widget):
     pass
