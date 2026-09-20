f57f9b82251a14765f4a16c4f7aa2e83c5c4c4d4
Ivan Slivenko <umanproger@ukr.net>
2026-09-20 11:29:10 +0300

    20-09-2026-1

---
+++ Lessons about Kivy from John Elder_2/31/lesson_31.py
@@ -0,0 +1,31 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.core.spelling import Spelling
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class FirstWindow(Screen):
+    pass
+
+class SecondWindow(Screen):
+    pass
+
+
+#Set the app size
+Window.size = (700,500)
+
+Builder.load_file('new_window_31.kv')
+
+class MyLayout(Widget):
+    pass
+class AwesomeApp(App):
+    def build(self):
+        Window.clearcolor = (0,0,0,0)
+        return MyLayout()
+
+if __name__ == "__main__":
+    AwesomeApp().run()
