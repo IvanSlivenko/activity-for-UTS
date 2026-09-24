89d717e29acc02c834e47a640324480851240967
Ivan Slivenko <umanproger@ukr.net>
2026-09-24 22:55:52 +0300

    24-09-2026-1

---
+++ Lessons about Kivy from John Elder_2/34/lesson_34.py
@@ -0,0 +1,24 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import  ObjectProperty
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.tabbedpanel import TabbedPanel
+
+#Set the app size
+Window.size = (700,500)
+
+Builder.load_file('tabs_34.kv')
+
+class MyLayout(TabbedPanel):
+    pass
+
+
+class AwesomeApp(App):
+    def build(self):
+        Window.clearcolor = (0,0,0,0)
+        return MyLayout()
+
+if __name__ == "__main__":
+    AwesomeApp().run()
